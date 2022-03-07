0a8049b84627d6749d8d94b70f568b4e3cc14958
Diego Mus <dmus@popoyan.com>
2022-03-06 21:32:06 -0600

    Add documentation Arduino Code

--- arduino/lectura.ino/lectura.ino.ino
+++ arduino/lectura.ino/lectura.ino.ino
@@ -1,21 +1,27 @@
-char valor;
+/*
+Practica de X Electrónica Analógica
+Universidad Mariano Galvez de Guatemala
+Sede Cobán Plan Fin de Semana
+*/
+
+char valor; //Variable para guardar caracteres recibidos
 int salida = 13;
 
 
 void setup(){
-    Serial.begin(9600);
-    pinMode(salida, OUTPUT);
+    Serial.begin(9600); //Velocidad de comunicación verficar que sea la misma que en la app de node
+    pinMode(salida, OUTPUT); // Salida que accionará el Transistor y Relé
 }
 
 void loop(){
-  if (Serial.available() > 0){
-      valor = Serial.read();
+  if (Serial.available() > 0){ //validador de trabajo de lectura serial
+      valor = Serial.read(); //Guardado del valor serial recibido
         switch(valor){
-            case '1' : 
+            case '1' :  //Opción de encendido
               digitalWrite(salida, HIGH);
               break;
             
-            default:
+            default: //Todo parametro enviado por serial que no tenga condición apaga el LED esto puede mejorarlo
               digitalWrite(salida,LOW);
               break;
           }
